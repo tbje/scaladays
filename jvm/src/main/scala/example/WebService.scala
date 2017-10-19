69fb825c3e593dcb2c54eca605e95c8f205e59d1
Trond Sandnes Bjerkestrand <trond@groosker.com>
2017-10-19 13:54:21 +0200

    Adding SSE support server side.

--- jvm/src/main/scala/example/WebService.scala
+++ jvm/src/main/scala/example/WebService.scala
@@ -15,6 +15,18 @@ import akka.http.scaladsl.server.Route
 import akka.stream.Materializer
 import akka.actor.ActorSystem
 
+import akka.http.scaladsl.marshalling.sse.EventStreamMarshalling._
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.http.scaladsl.model.sse.ServerSentEvent
+import scala.concurrent.duration._
+
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter.ISO_LOCAL_TIME
+
 class WebService() extends Directives {
   def apply()(implicit s: ActorSystem, m: Materializer, e: ExecutionContext): Route = {
 
@@ -42,5 +54,17 @@ class WebService() extends Directives {
     } ~
     path("api" / Segments) { segments =>
       post(AutowireServer.dispatch(segments))
-    }  }
+    } ~
+    path("events") {
+      get {
+        complete {
+          Source
+            .tick(2.seconds, 2.seconds, NotUsed)
+            .map(_ => LocalTime.now())
+            .map(time => ServerSentEvent(ISO_LOCAL_TIME.format(time)))
+            .keepAlive(1.second, () => ServerSentEvent.heartbeat)
+        }
+      }
+    }
+  }
 }
