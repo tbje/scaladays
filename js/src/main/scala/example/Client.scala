de8db212c2edb9c256251e88a1d7f54f8faced62
Trond Sandnes Bjerkestrand <trond@groosker.com>
2018-01-31 17:52:25 +0100

    Some drawings.

--- js/src/main/scala/example/Client.scala
+++ js/src/main/scala/example/Client.scala
@@ -4,13 +4,6 @@ import org.scalajs.dom.ext.KeyCode
 import org.scalajs.dom.raw.{ CanvasRenderingContext2D => Ctx, MessageEvent }
 import org.scalajs.dom
 import scalatags.JsDom.all._
-import scala.util._
-import autowire._
-import boopickle.Default._
-
-import concurrent.ExecutionContext.Implicits.global
-import shared._
-import shared.MyType._
 
 object Client {
 
@@ -18,79 +11,29 @@ object Client {
 
     val devDiv = Presentation.init(dom.window.location.pathname)
 
-    val d = div(margin:="10px").render
-    devDiv.appendChild(d).render
+    val heading = h1("Good afternoon ScalaDays!").render
 
-    Wire[shared.Api].getUser().call().onComplete {
-      case Success(Person(name, age)) =>
-        val msg = if (age < 18)
-          h1(s"Sorry $name, only adults allowed here", color:="red")
-        else
-          h1(s"Welcome $name, please enjoy!", color:="green")
-        d.appendChild(msg.render)
-      case Failure(f) =>
-        println("Failed call" + f)
-        d.appendChild(h1("Not able to contact server", color:="red").render)
-    }
-    def formGroup(t: String) = {
-      val theId = t.toLowerCase()
-      val i = input(`class`:="form-control", id:=theId).render
-      val err = div(`class`:="invalid-feedback", "").render
-      val fg = div(`class`:="form-group", label(`for`:=theId, t), i, err).render
-      (i, fg, err)
-    }
-    val success = span(color:="green").render
-    val (i, fg, errName) = formGroup("Name")
-    val (i2, fg2, errAge) = formGroup("Age")
-    val b = button(`class`:="btn btn-primary", marginRight:= 10, `type`:="submit", "Add user").render
-    val f = form(width:="70%", marginLeft:=30,
-      fg, fg2, b, success).render
+    val t = dom.window
+    implicit val canCtx = CanvasCtx(t.innerWidth, t.innerHeight)
+    devDiv.appendChild(heading)
+    devDiv.appendChild(canCtx.canvas)
 
-    f.onsubmit = { e => {onsubmit; false} }
+    case class Point(x: Double, y: Double)
 
-    val error = span(id:="error", color:="red").render
-    devDiv.appendChild(f)
-    devDiv.appendChild(error)
-
-    b.onclick = {(e) => submit()}
-    def submit() = {
-      error.innerHTML = ""
-      i.setAttribute("class", "form-control")
-      i2.setAttribute("class", "form-control")
-      Validation.validatePerson(i.value, i2.value) match {
-        case Right(p) =>
-          Wire[shared.Api].addUser(p).call().map {
-            case Some(p) =>
-              success.innerHTML = s"User added: $p!"
-              i.value = ""
-              i2.value = ""
-            case None =>
-              success.innerHTML = "Something went wrong"
-            case o => println(o)
-          }
-        case Left((name, age)) =>
-          name match {
-            case Some(err) =>
-              i.setAttribute("class", "is-invalid form-control")
-              errName.innerHTML = err
-            case _ =>
-          }
-          age match {
-            case Some(err) =>
-              i2.setAttribute("class", "is-invalid form-control")
-              errAge.innerHTML = err
-            case _ =>
-          }
+    case class Rect(width: Int, height: Int)  {
+      def draw(point: Point, color: Color)(implicit c: CanvasCtx) = {
+        val ctx = c.ctx
+        ctx.beginPath()
+        ctx.moveTo(point.x, point.y)
+        ctx.lineTo(point.x + width, point.y)
+        ctx.lineTo(point.x + width, point.y + height)
+        ctx.lineTo(point.x, point.y + height)
+        ctx.closePath()
+        color.fill.foreach(ctx.fillStyle = _)
+        ctx.fill()
       }
     }
-//    Wire[shared.Api].getFromServer().call().onComplete {
-//      case Success(m) =>
-//        val msg = s"Hi I'm ${m.name}"
-//        println(msg)
-//        devDiv.textContent = msg
-//      case Failure(f) =>
-//        println("Failed call" + f)
-//        devDiv.textContent = "Not able to contact server"
-//    }
+    Rect(100, 100).draw(Point(100, 100), Color.rand())
+
   }
 }
