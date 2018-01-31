d3e1dab1d3cb2a3819113e97ad6a2a7b84737567
Trond Sandnes Bjerkestrand <trond@groosker.com>
2018-01-31 17:58:37 +0100

    More colors

--- js/src/main/scala/example/Client.scala
+++ js/src/main/scala/example/Client.scala
@@ -13,27 +13,63 @@ object Client {
 
     val heading = h1("Good afternoon ScalaDays!").render
 
+    devDiv.appendChild(heading)
+
     val t = dom.window
     implicit val canCtx = CanvasCtx(t.innerWidth, t.innerHeight)
-    devDiv.appendChild(heading)
     devDiv.appendChild(canCtx.canvas)
 
     case class Point(x: Double, y: Double)
 
-    case class Rect(width: Int, height: Int)  {
-      def draw(point: Point, color: Color)(implicit c: CanvasCtx) = {
-        val ctx = c.ctx
+    sealed trait Shape {
+      def draw(point: Point, color: Color)(implicit ctx: CanvasCtx): Unit
+      def drawWith(ctx: Ctx, point: Point, color: Color)(func: => Unit) = {
         ctx.beginPath()
-        ctx.moveTo(point.x, point.y)
-        ctx.lineTo(point.x + width, point.y)
-        ctx.lineTo(point.x + width, point.y + height)
-        ctx.lineTo(point.x, point.y + height)
+        color.applyColor(ctx)
+        func
         ctx.closePath()
-        color.fill.foreach(ctx.fillStyle = _)
         ctx.fill()
+        ctx.stroke()
       }
     }
-    Rect(100, 100).draw(Point(100, 100), Color.rand())
+
+    case class Circle(radius: Double) extends Shape {
+      def draw(point: Point, color: Color)(implicit ctx: CanvasCtx) = {
+        drawWith(ctx.ctx, point, color){
+          ctx.ctx.arc(point.x, point.y, radius, 0, 2 * math.Pi)
+        }
+      }
+    }
+
+    case class Rect(width: Int, height: Int) extends Shape {
+      def draw(point: Point, color: Color)(implicit ctx: CanvasCtx) = {
+        drawWith(ctx.ctx, point, color){
+          ctx.ctx.moveTo(point.x, point.y)
+          ctx.ctx.lineTo(point.x + width, point.y)
+          ctx.ctx.lineTo(point.x + width, point.y + height)
+          ctx.ctx.lineTo(point.x, point.y + height)
+        }
+      }
+    }
+
+    case class Triangle(width: Int, height: Int) extends Shape {
+      def draw(point: Point, color: Color)(implicit ctx: CanvasCtx) = {
+        drawWith(ctx.ctx, point, color){
+          ctx.ctx.moveTo(point.x + (width / 2), point.y)  //top
+          ctx.ctx.lineTo(point.x, point.y + height) // bottom left
+          ctx.ctx.lineTo(point.x + width, point.y + height) // bottom right
+        }
+      }
+    }
+
+    import scala.util.Random.nextInt
+
+    def randomPoint() =
+      Point(nextInt(canCtx.width.toInt).toDouble, nextInt(canCtx.height.toInt).toDouble)
+
+    Triangle(300, 200).draw(randomPoint(), Color.rand)
+    Circle(100).draw(randomPoint(), Color.rand)
+    Rect(100, 50).draw(randomPoint(), Color.rand)
 
   }
 }
